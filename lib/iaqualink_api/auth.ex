fa7300738ec7ba2c4e560ea09c3864134ffc8f6b
Andrew Kraut <akraut@gmail.com>
2022-05-10 14:16:40 -0700

    Fix payload format for Jason

--- lib/iaqualink_api/auth.ex
+++ lib/iaqualink_api/auth.ex
@@ -1,9 +1,9 @@
 defmodule IaqualinkApi.Auth do
   def login do
     payload = %{
-      email: Application.get_env(:iaqualink_api, :email),
-      password: Application.get_env(:iaqualink_api, :password),
-      language: "en"
+      "email" => Application.get_env(:iaqualink_api, :email),
+      "password" => Application.get_env(:iaqualink_api, :password),
+      "language" => "en"
     }
 
     url = Application.get_env(:iaqualink_api, :login_url)
