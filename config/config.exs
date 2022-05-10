d9021ed24ea4dd2dcb8645406b28c6f7cc72d3fc
Andrew Kraut <akraut@gmail.com>
2022-05-09 19:54:12 -0700

    Update Config with other URLs

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,8 @@
 import Config
 
 config :iaqualink_api,
-  url: "https://prod.zodiac-io.com/users/v1/login",
+  login_url: "https://prod.zodiac-io.com/users/v1/login",
+  api_url: "https://prm.iaqualink.net/v2/",
   language: "en"
 
 import_config "config.secret.exs"
