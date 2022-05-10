a8ea6eff1c12a8e0ea2de75263acf31645e9f532
Andrew Kraut <akraut@gmail.com>
2022-05-09 19:54:45 -0700

    Assemble Finch Request for login

--- lib/iaqualink_api/auth.ex
+++ lib/iaqualink_api/auth.ex
@@ -1,2 +1,22 @@
 defmodule IaqualinkApi.Auth do
+  def login do
+    payload = %{
+      email: Application.get_env(:iaqualink_api, :email),
+      password: Application.get_env(:iaqualink_api, :password),
+      language: "en"
+    }
+
+    url = Application.get_env(:iaqualink_api, :login_url)
+
+    headers = [{"content-type", "application/json"}]
+
+    request = Finch.build(
+      :post,
+      url,
+      headers,
+      payload
+    )
+
+    request
+  end
 end
