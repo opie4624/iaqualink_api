7759e39afe189fd72428245ebb4864525506ec47
Andrew Kraut <akraut@gmail.com>
2022-05-10 15:54:20 -0700

    Parse result from login

--- lib/iaqualink_api/auth.ex
+++ lib/iaqualink_api/auth.ex
@@ -11,8 +11,16 @@ defmodule IaqualinkApi.Auth do
     headers = [{"content-type", "application/json"}]
 
     with {:ok, response} = Finch.build(:post, url, headers, Jason.encode!(payload))
-    |> Finch.request(ApiFinch),
-    {:ok, result} = Jason.decode(response.body),
-    do: {:ok, result}
+         |> Finch.request(ApiFinch),
+         {:ok, decoded} = Jason.decode(response.body),
+           do: %{
+             renewUrl: Map.get(decoded, "cognitoPool") |> Map.get("domain"),
+             clientId: Map.get(decoded, "cognitoPool") |> Map.get("appClientId"),
+             sessionToken: Map.get(decoded, "credentials") |> Map.get("SessionToken"),
+             accessToken: Map.get(decoded, "userPoolOAuth") |> Map.get("AccessToken"),
+             refreshToken: Map.get(decoded, "userPoolOAuth") |> Map.get("RefreshToken"),
+             idToken: Map.get(decoded, "userPoolOAuth") |> Map.get("IdToken"),
+             expires: Map.get(decoded, "userPoolOAuth") |> Map.get("ExpiresIn")
+           }
   end
 end
