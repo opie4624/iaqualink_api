6ddbf223ee6b73fd5ca4284bda3935ba5ea7f3d1
Andrew Kraut <akraut@gmail.com>
2022-05-10 22:11:45 -0700

    Fetch session info

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :iaqualink_api,
   login_url: "https://prod.zodiac-io.com/users/v1/login",
-  api_url: "https://prm.iaqualink.net/v2/",
+  api_url: "https://prm.iaqualink.net/v2",
   language: "en"
 
 import_config "config.secret.exs"
--- lib/iaqualink_api/account.ex
+++ lib/iaqualink_api/account.ex
@@ -1,2 +1,32 @@
 defmodule IaqualinkApi.Account do
+  alias IaqualinkApi.Auth.Session
+
+  def get_user_id do
+    get_session_info()
+    |> Map.get("session_user_id")
+  end
+
+  def get_session_id do
+    get_session_info()
+    |> Map.get("session_id")
+  end
+
+  defp make_headers do
+    id_token = Session.get(:id_token)
+
+    [{"authorization", "Bearer #{id_token}"}]
+  end
+
+  defp make_url(destination) do
+    base_url = Application.get_env(:iaqualink_api, :api_url)
+    "#{base_url}/#{destination}"
+  end
+
+  defp get_session_info do
+    with {:ok, response} <-
+           Finch.build(:get, make_url("userId"), make_headers())
+           |> Finch.request(ApiFinch),
+         {:ok, decoded} <- Jason.decode(response.body),
+         do: decoded
+  end
 end
