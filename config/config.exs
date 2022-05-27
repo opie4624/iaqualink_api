d6dec64b82657213034abf27dca63f67ba4dd5d7
Andrew Kraut <akraut@gmail.com>
2022-05-26 18:06:32 -0700

    Add additional api endpoints

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,9 @@ import Config
 config :iaqualink_api,
   login_url: "https://prod.zodiac-io.com/users/v1/login",
   api_url: "https://prm.iaqualink.net/v2",
+  p_api_url: "https://p-api.iaqualink.net/v1/mobile",
+  r_api_url: "https://r-api.iaqualink.net",
+  api_key: "EOOEMOW4YR6QNB07",
   language: "en"
 
 import_config "config.secret.exs"
