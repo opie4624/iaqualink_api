776430fc4d7fc1759b093862b7e3f8578d9024b3
Andrew Kraut <akraut@gmail.com>
2022-05-26 18:08:22 -0700

    Get device list

--- lib/iaqualink_api/account.ex
+++ lib/iaqualink_api/account.ex
@@ -23,14 +23,33 @@ defmodule IaqualinkApi.Account do
          do: Map.get(decoded, "locations")
   end
 
+  def get_devices do
+    user_id = get_user_id()
+    api_key = Application.get_env(:iaqualink_api, :api_key)
+    auth_token = Session.get(:auth_token)
+
+    devices =
+      with {:ok, response} <-
+             Finch.build(
+               :get,
+               make_url(
+                 "devices.json?api_key=#{api_key}&authentication_token=#{auth_token}&user_id=#{user_id}",
+                 :r_api_url
+               )
+             )
+             |> Finch.request(ApiFinch),
+           {:ok, decoded} = Jason.decode(response.body),
+           do: decoded
+  end
+
   defp make_headers do
     id_token = Session.get(:id_token)
 
     [{"authorization", "Bearer #{id_token}"}]
   end
 
-  defp make_url(destination) do
-    base_url = Application.get_env(:iaqualink_api, :api_url)
+  defp make_url(destination, url_base \\ :api_url) do
+    base_url = Application.get_env(:iaqualink_api, url_base)
     "#{base_url}/#{destination}"
   end
 
